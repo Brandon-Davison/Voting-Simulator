ab2ed76678e2551efce03358ca310fa5d887796c
Brandon-Davison <Brandon_davison@outlook.com>
2019-04-21 15:27:20 -0700

    Added PermissionManager code to get storage permission. Internet permisssion in manifest

--- app/src/main/java/brandon/davison/com/voting/MainActivity.java
+++ app/src/main/java/brandon/davison/com/voting/MainActivity.java
@@ -1,13 +1,28 @@
 package brandon.davison.com.voting;
 
+import android.Manifest;
+import android.os.storage.StorageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import brandon.davison.com.voting.permissions.PermissionManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String log = "MainActivity ErrorCode 1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PermissionManager permissionManager = new PermissionManager(getApplicationContext(), MainActivity.this);
+
+        Toast.makeText(MainActivity.this, "onStart", Toast.LENGTH_LONG).show();
+
+        permissionManager.requestStoragePermission();
+        Log.d("PermissionManager", "Storage = " + permissionManager.readStorageIsAllowed());
     }
 }
--- app/src/main/java/brandon/davison/com/voting/permissions/PermissionManager.java
+++ app/src/main/java/brandon/davison/com/voting/permissions/PermissionManager.java
@@ -1,4 +1,39 @@
 package brandon.davison.com.voting.permissions;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
+
 public class PermissionManager {
+
+    private static final int STORAGE_PERMISSION_CODE = 23;
+
+    private Context context;
+    private Activity activity;
+
+    public PermissionManager(Context context, Activity activity) {
+        this.context = context;
+        this.activity = activity;
+    }
+
+    public boolean readStorageIsAllowed() {
+        final int permissionStatus = ContextCompat.checkSelfPermission(context,
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+        return permissionStatus == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public void requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
+                Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            Toast.makeText(activity.getApplicationContext(),
+                    "App can't function unless permission is granted.",
+                    Toast.LENGTH_LONG).show();
+        }
+        ActivityCompat.requestPermissions(activity, new String[]
+                {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+    }
 }
