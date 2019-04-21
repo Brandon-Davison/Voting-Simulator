ee81026786d1b9a99c3acf3c89fe7cd79e8c4866
Brandon-Davison <Brandon_davison@outlook.com>
2019-04-21 16:01:02 -0700

    VoteSettings implementation

--- app/src/main/java/brandon/davison/com/voting/VoteSettings.java
+++ app/src/main/java/brandon/davison/com/voting/VoteSettings.java
@@ -1,4 +1,30 @@
 package brandon.davison.com.voting;
 
+import java.util.ArrayList;
+
+import brandon.davison.com.voting.users.Candidate;
+
 public class VoteSettings {
+
+    private ArrayList<Candidate> candidates;
+    private int votesAvailable, votesToWin;
+
+    public VoteSettings(int votesToWin, int votesAvailable) {
+        this.votesToWin = votesToWin;
+        this.votesAvailable = votesAvailable;
+        candidates = new ArrayList<>();
+    }
+
+    public ArrayList<Candidate> getCandidates() {
+        return candidates;
+    }
+
+    public int getVotesAvailable() {
+        return votesAvailable;
+    }
+
+    public int getVotesToWin() {
+        return votesToWin;
+    }
+
 }
