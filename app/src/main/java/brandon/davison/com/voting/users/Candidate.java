388ae0c6fcea403d18ccbad176cd4bebec9b4b84
Brandon-Davison <Brandon_davison@outlook.com>
2019-04-21 15:44:58 -0700

    Candidate class implementation

--- app/src/main/java/brandon/davison/com/voting/users/Candidate.java
+++ app/src/main/java/brandon/davison/com/voting/users/Candidate.java
@@ -1,7 +1,59 @@
 package brandon.davison.com.voting.users;
 
+import java.util.ArrayList;
+
 public class Candidate {
 
-    
+    private String name;
+    private int id;
+    private int votesNeeded, votesReceived;
+    private boolean hasWon;
+    private ArrayList<Voter> voters;
+
+    public Candidate(String name, int id, int votesNeeded) {
+        this.name = name;
+        this.id = id;
+        this.votesNeeded = votesNeeded;
+        votesReceived = 0;
+        hasWon = false;
+        voters = new ArrayList<>();
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getid() {
+        return id;
+    }
+
+    public int getVotesNeeded() {
+        return votesNeeded;
+    }
+
+    public int getVotesReceived() {
+        return votesReceived;
+    }
+
+    public boolean hasWon() {
+        return hasWon;
+    }
+
+    public ArrayList<Voter> getVoters() {
+        return voters;
+    }
+
+    // Adds vote and returns true if candidate won election
+    public boolean addVote(Voter voter) {
+        voters.add(voter);
+        votesReceived++;
+
+        if (votesReceived == votesNeeded) {
+            hasWon = true;
+            return true;
+        }
+        return false;
+    }
 
 }
