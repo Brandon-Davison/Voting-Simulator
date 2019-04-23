375af2633686b227e5f279e85336ebc2ee160be1
Brandon-Davison <Brandon_davison@outlook.com>
2019-04-23 12:46:52 -0700

    Added IdManager class and skeleton to generate, read, and write login credential ids

--- app/src/main/java/brandon/davison/com/voting/Auth/IdManager.java
+++ app/src/main/java/brandon/davison/com/voting/Auth/IdManager.java
@@ -1,4 +1,44 @@
 package brandon.davison.com.voting.Auth;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
+import brandon.davison.com.voting.voting.VoteSettings;
+
 public class IdManager {
+
+    private ArrayList<UUID> ids = new ArrayList<>();
+
+    public IdManager(VoteSettings settings) {
+        if (!settings.getStarted()) { // start new election and generate login id keys
+            generateIds(settings.getVotesAvailable());
+        } else { // election is ongoing. Read in IDS from database
+
+        }
+    }
+
+    // Returns if an ID is in the list of generated IDS
+    public boolean isValidID(UUID id) {
+        if (ids.size() == 0) return false;
+
+        return ids.contains(id);
+    }
+
+    // Read in generated ids from database
+    public void readIds(int numberIds) {
+
+    }
+
+    // Generate login id keys
+    private void generateIds(int numberIds) {
+        for (int i = 0; i < numberIds; i++) {
+            ids.add(UUID.randomUUID());
+        }
+    }
+
+    // Write generated IDS to database
+    private void writeIds(ArrayList<UUID> ids) {
+
+    }
+
 }
