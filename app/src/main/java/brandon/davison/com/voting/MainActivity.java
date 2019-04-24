a86b60a9548a3d60e7294e626929bab022c1c743
Brandon-Davison <Brandon_davison@outlook.com>
2019-04-24 10:23:00 -0700

    Populates Spinner with Candidate names

--- app/src/main/java/brandon/davison/com/voting/MainActivity.java
+++ app/src/main/java/brandon/davison/com/voting/MainActivity.java
@@ -4,8 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import brandon.davison.com.voting.permissions.PermissionManager;
 import brandon.davison.com.voting.users.Candidate;
@@ -18,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     /* UI views */
     private TextView voterNameView, voterIdView;
+    private Spinner spinner;
 
     /* Non-UI variables */
 
@@ -30,12 +39,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setupViews();
 
+
         PermissionManager permissionManager = new PermissionManager(getApplicationContext(), MainActivity.this);
         permissionManager.requestStoragePermission();
 
         Intent intent = getIntent();
         final String voterId = intent.getStringExtra("voterID").substring(0, 5);
-        int candidates = intent.getIntExtra("candidates", -1);
+        final int candidates = intent.getIntExtra("candidates", -1);
         boolean started = intent.getBooleanExtra("started", true);
         int votesAvailable = intent.getIntExtra("votesAvailable", -1);
         int votesToWin = intent.getIntExtra("votesToWin", -1);
@@ -59,22 +69,21 @@ public class MainActivity extends AppCompatActivity {
                         public void run() {
                             voterNameView.setText(voter.getName());
                             voterIdView.setText(voter.getId().substring(0, 5));
+
+                            List<String> names = new ArrayList<>();
+                            for (Candidate candidate : candidateManager.getCandidates()) {
+                                names.add(candidate.getName());
+                            }
+
+                            ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(MainActivity.this,
+                                    android.R.layout.simple_spinner_item, names);
+                            dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                            spinner.setAdapter(dataAdapter);
                         }
                     });
                 } else {
                     Toast.makeText(MainActivity.this, "Error, could not find voter",
                             Toast.LENGTH_LONG).show();
-                    return;
-                }
-
-                Log.d("candidateTesting", "Voter size: " + voterManager.getVoters().size());
-                Log.d("candidateTesting", "ID: " + voterId);
-                for (Candidate candidate : candidateManager.getCandidates()) {
-                    Log.d("candidateTesting", "From Main");
-                    Log.d("candidateTesting", candidate.getName());
-                    Log.d("candidateTesting", " " + candidate.getid());
-                    Log.d("candidateTesting", " " + candidate.getVotesReceived());
-                    Log.d("candidateTesting", " " + candidate.hasWon());
                 }
             }
         });
@@ -84,5 +93,19 @@ public class MainActivity extends AppCompatActivity {
     private void setupViews() {
         voterNameView = findViewById(R.id.voter_name);
         voterIdView = findViewById(R.id.voter_id);
+        spinner = findViewById(R.id.spinner);
+
+    }
+
+    private void runCandidateDataTests(VoterManager voterManager, CandidateManager candidateManager, int voterId) {
+        Log.d("candidateTesting", "Voter size: " + voterManager.getVoters().size());
+        Log.d("candidateTesting", "ID: " + voterId);
+        for (Candidate candidate : candidateManager.getCandidates()) {
+            Log.d("candidateTesting", "From Main");
+            Log.d("candidateTesting", candidate.getName());
+            Log.d("candidateTesting", " " + candidate.getid());
+            Log.d("candidateTesting", " " + candidate.getVotesReceived());
+            Log.d("candidateTesting", " " + candidate.hasWon());
+        }
     }
 }
